75c803b47eb2c5a210af2e76769ee2a1cea4e71e
jhleeO <jhlee@olulo.io>
2021-06-02 14:36:41 +0900

    middleware

--- src/server.js
+++ src/server.js
@@ -4,6 +4,11 @@ const PORT = 4000;
 
 const app = express();
 
+const gossipMiddleware = (req, res, next) => {
+  console.log(`Someone is going to : ${req.url}`);
+  next();
+}
+
 const handleHome = (req, res) => {
   return res.send("Good to see you!");
 }
@@ -11,7 +16,7 @@ const handleHome = (req, res) => {
 const handleLogin = (req, res) => {
   return res.send("Login here!");
 }
-app.get("/", handleHome);
+app.get("/", gossipMiddleware, handleHome);
 app.get("/login", handleLogin);
 
 const handlerListen = () => console.log("✅ Server listening on port http://localhost:${PORT} ☄️")
