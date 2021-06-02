484408fe0ac515d820aff11a46fc32dcf774639b
jhleeO <jhlee@olulo.io>
2021-06-02 12:40:43 +0900

    response
    - end()
    - send()

--- src/server.js
+++ src/server.js
@@ -4,7 +4,15 @@ const PORT = 4000;
 
 const app = express();
 
-app.get("/", () => console.log("Somebody is trying to go home."));
+const handleHome = (req, res) => {
+  return res.send("Good to see you!");
+}
+
+const handleLogin = (req, res) => {
+  return res.send("Login here!");
+}
+app.get("/", handleHome);
+app.get("/login", handleLogin);
 
 const handlerListen = () => console.log("✅ Server listening on port http://localhost:${PORT} ☄️")
 
