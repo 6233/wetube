371639521f6f73eece37f92073339c2d8e09e721
jhleeO <jhlee@olulo.io>
2021-06-04 10:17:23 +0900

    Router

--- src/server.js
+++ src/server.js
@@ -5,32 +5,32 @@ const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
-
-const privateMiddlewware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not allowed</h1>");
-  }
-  
-  next();
-}
-
-const handleHome = (req, res) => {
-  return res.send("Good to see you!");
-}
-
-const handleLogin = (req, res) => {
-  return res.send("Login here!");
-}
-
-const handleProtected = (req, res) => {
-  return res.send("Welcome to the private handle.");
-}
-
 app.use(logger);
-app.get("/", handleHome);
-app.get("/login", handleLogin);
-app.get("/protected", handleProtected)
+
+
+// Router
+const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send("Home");
+
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User");
+
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
 
 const handlerListen = () => console.log(`✅ Server listening on port http://localhost:${PORT}☄️`)
 
