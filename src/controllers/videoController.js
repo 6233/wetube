fa1f4449db6cd27644d4cbb0636e13b52aed372e
jhleeO <jhlee@olulo.io>
2021-06-07 18:12:54 +0900

    pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,8 @@
 export const trending = (req, res) => {
-  return res.send(
-    `<!DOCTYPE html><html lang ='ko'><head><title>Wetube</title></head><body><h1>Home</h1><footer>&copy; 2021 Wetube - All rights reserved</footer></body></html>`
-    )
+  return res.render("home");
 }
 export const see = (req, res) => {
-  return res.send(
-    `<!DOCTYPE html><html lang ='ko'><head><title>Wetube</title></head><body><h1>Watch Video #${req.params.id}</h1><footer>&copy; 2021 Wetube - All rights reserved</footer></body></html>`
-  )
+  return res.render("watch")
 }
 export const edit = (req, res) => {
   return res.send(
