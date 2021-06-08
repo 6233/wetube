70ac6e6b0b1fbb653ea895137a4d17e0a6163ccd
jhleeO <jhlee@olulo.io>
2021-06-08 11:49:56 +0900

    Extending Templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,12 +2,10 @@ export const trending = (req, res) => {
   return res.render("home");
 }
 export const see = (req, res) => {
-  return res.render("watch")
+  return res.render("watch");
 }
 export const edit = (req, res) => {
-  return res.send(
-    `<!DOCTYPE html><html lang ='ko'><head><title>Wetube</title></head><body><h1>Edit Video #${req.params.id}</h1><footer>&copy; 2021 Wetube - All rights reserved</footer></body></html>`
-  )
+  return res.render("edit");
 }
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
