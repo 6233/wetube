78f135eb296a75e4338fd45eff7ddb24fb1c18aa
jhleeO <jhlee@olulo.io>
2021-06-14 18:18:52 +0900

    pre-Upload

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
-import {watch, getEdit, postEdit} from "../controllers/videoController";
+import {watch, getEdit, postEdit, getUpload, postUpload} from "../controllers/videoController";
 
 const videosRouter = express.Router();
 
 videosRouter.get("/:id(\\d+)", watch);
 videosRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videosRouter.route("/upload").get(getUpload).post(postUpload);
 
 
 export default videosRouter;
